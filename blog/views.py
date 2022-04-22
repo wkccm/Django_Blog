650ff3dccdaf1fa883a4148c0b9b6c17392e64f1
wkccm <1073385724@qq.com>
2022-04-22 16:07:45 +0800

    to CentOS

---
+++ blog/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Post, Tag
+import markdown
+import re
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from django.views.generic import ListView, DetailView
+from pure_pagination.mixins import PaginationMixin
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.db.models import Q
+
+# Create your views here.
+
+
+class IndexView(PaginationMixin, ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__year=year, created_time__month=month)
+        )
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(tags=t)
+
+
+def search(request):
+    q = request.GET.get('q')
+
+    if not q:
+        error_msg = "请输入搜索关键词"
+        messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
+        return redirect('blog:index')
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'post_list': post_list})
